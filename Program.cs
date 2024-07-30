a60d2d2836d0f42370a38496f7959bbd00c48fdc
rrit5727 <rrit5727@gmail.com>
2024-07-30 22:41:19 +1000

    update folder structure, configure entity framework core to use in Memory db

--- Program.cs
+++ Program.cs
@@ -1,19 +1,38 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TransactionApi.Data;
+using TransactionApi.Repositories;
+using TransactionApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers(); // Add this for MVC/Web API controllers
-builder.Services.AddAuthorization(); // Add this for authorization
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure Entity Framework Core to use In-Memory database
+builder.Services.AddDbContext<TransactionContext>(options =>
+    options.UseInMemoryDatabase("TransactionDb"));
+
+// Register repository and service
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseHttpsRedirection();
