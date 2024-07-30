e1ce262f66634f5603b86cd69230ccc8cc06753b
rrit5727 <rrit5727@gmail.com>
2024-07-30 22:19:42 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers(); // Add this for MVC/Web API controllers
+builder.Services.AddAuthorization(); // Add this for authorization
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers(); // Map controllers
+
+// Add a simple default route
+app.MapGet("/", () => "Welcome to the Transaction API!");
+
+app.Run();
