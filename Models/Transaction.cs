b5f2dca2967d47dd99ee3990253592abfe28b2d9
rrit5727 <rrit5727@gmail.com>
2024-07-30 23:14:49 +1000

    add transactions.cs Model

---
+++ Models/Transaction.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations; 
+
+namespace TransactionApi.Models
+{
+  public class Transaction
+  {
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime DateTime {get; set; }
+
+    [Required]
+    public string Description {get; set; } = string.empty;
+
+    [Required]
+    public decimal Amount {get; set; }
+
+    [Required]
+    public string Status {get; set; } = string.empty
+  }
+}
