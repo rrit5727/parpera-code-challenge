130be49a151af8eed792e69917e2fcdbc34ad1c5
rrit5727 <rrit5727@gmail.com>
2024-07-31 21:46:33 +1000

    add transaction controller

---
+++ Controllers/TransactionsControllers.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using TransactionApi.Models;
+using TransactionApi.Services;
+
+namespace TransactionApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class TransactionsController : ControllerBase
+  {
+    private readonly ITransactionService _service;
+
+    public TransactionsController(ITransactionService service)
+    {
+      _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+    {
+      var transactions = await _service.GetAllTransactionsAsync();
+      return Ok(transactions);
+    }
+
+    [HttpPut("{id}/status")]
+    public async Task<IActionResult> UpdateTransactionStatus(int id, [FromBody] string newStatus)
+    {
+      var Transaction = await _service.UpdateTransactionStatusAsync(id, newStatus)
+      if (transaction == null)
+      {
+        return NotFound();
+      }
+      return Ok(transaction)
+    }
+
+  }
+
+
+
+}
